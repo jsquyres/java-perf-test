2ff0440a57184d3a5f2b8cc04dd75339c2229767
Jeff Squyres <jsquyres@cisco.com>
2014-06-01 03:53:25 -0700

    Only use specific networks

--- sbatch-c-runner.sh
+++ sbatch-c-runner.sh
@@ -6,7 +6,7 @@ np=$3
 
 nservers=$SLURM_NNODES
 
-cmd="mpirun --mca btl $btl --bind-to core --map-by node -np $np $benchmark"
+cmd="mpirun --mca btl_usnic_if_include 10.1.0.0/16,10.2.0.0/16 --mca btl_tcp_if_exclude 127.0.0.0/8,10.4.0.0/16 --mca btl $btl --bind-to core --map-by node -np $np $benchmark"
 
 cat <<EOF
 Running C benchmark $benchmark, class $2, on $nservers servers (np=$np), $btl
--- sbatch-java-runner.sh
+++ sbatch-java-runner.sh
@@ -7,7 +7,7 @@ np=$4
 
 nservers=$SLURM_NNODES
 
-cmd="mpirun --mca btl $btl --bind-to core --map-by node -np $np java NPB_MPJ.$benchmark class=$class"
+cmd="mpirun --mca btl_usnic_if_include 10.1.0.0/16,10.2.0.0/16 --mca btl_tcp_if_exclude 127.0.0.0/8,10.4.0.0/16 --mca btl $btl --bind-to core --map-by node -np $np java NPB_MPJ.$benchmark class=$class"
 
 cat <<EOF
 Running Java benchmark $benchmark, class $2, on $nservers servers (np=$np), $btl
