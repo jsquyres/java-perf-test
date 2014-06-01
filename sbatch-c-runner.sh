79aead597fd0d31785335f643a0c925276d85f01
Jeff Squyres <jsquyres@cisco.com>
2014-06-01 04:29:01 -0700

    Only include 2 networks for both tcp and usnic

--- sbatch-c-runner.sh
+++ sbatch-c-runner.sh
@@ -6,7 +6,10 @@ np=$3
 
 nservers=$SLURM_NNODES
 
-cmd="mpirun --mca btl_usnic_if_include 10.1.0.0/16,10.2.0.0/16 --mca btl_tcp_if_exclude 127.0.0.0/8,10.4.0.0/16 --mca btl $btl --bind-to core --map-by node -np $np $benchmark"
+networks="10.10.0.0/16,10.2.0.0/16"
+mca_networks="--mca btl_usnic_if_include $networks --mca btl_tcp_if_include $networks"
+
+cmd="mpirun $mca_networks --mca btl $btl --bind-to core --map-by node -np $np $benchmark"
 
 cat <<EOF
 Running C benchmark $benchmark, class $2, on $nservers servers (np=$np), $btl
--- sbatch-java-runner.sh
+++ sbatch-java-runner.sh
@@ -7,7 +7,10 @@ np=$4
 
 nservers=$SLURM_NNODES
 
-cmd="mpirun --mca btl_usnic_if_include 10.1.0.0/16,10.2.0.0/16 --mca btl_tcp_if_exclude 127.0.0.0/8,10.4.0.0/16 --mca btl $btl --bind-to core --map-by node -np $np java NPB_MPJ.$benchmark class=$class"
+networks="10.10.0.0/16,10.2.0.0/16"
+mca_networks="--mca btl_usnic_if_include $networks --mca btl_tcp_if_include $networks"
+
+cmd="mpirun $mca_networks --mca btl $btl --bind-to core --map-by node -np $np java -Xmx64g NPB_MPJ.$benchmark class=$class"
 
 cat <<EOF
 Running Java benchmark $benchmark, class $2, on $nservers servers (np=$np), $btl
